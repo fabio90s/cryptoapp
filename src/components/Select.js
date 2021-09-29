6ca73249266e106006f282b4caafa04ee3a44af3
Fabio Catino <fabivs9@gmail.com>
2021-09-29 12:14:16 +0200

    list for select component alphabetically sorted

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
@@ -6,12 +6,20 @@ import { useGetAllCryptosQuery } from '../services/cryptoApi';
 
 export default function NewsSelect(props) {
 	const { data: cryptosList, isFetching } = useGetAllCryptosQuery();
-	const [placeholder, setPlaceholder] = React.useState('');
+	const [placeholder, setPlaceholder] = useState('');
 
 	const handleChange = (event) => {
 		setPlaceholder(event.target.value);
 		props.onClick(placeholder);
 	};
+	let sortedList = [];
+
+	for (let i in cryptosList?.data.coins) {
+		let temp = {
+			name: cryptosList?.data.coins[i].name,
+		};
+		sortedList.push(temp);
+	}
 
 	return (
 		<>
@@ -38,15 +46,17 @@ export default function NewsSelect(props) {
 							<MenuItem style={{ display: 'flex' }} key={1990} value={'All'}>
 								{'All'}
 							</MenuItem>
-							{cryptosList?.data?.coins?.map((item) => (
-								<MenuItem
-									style={{ display: 'flex' }}
-									key={item.name}
-									value={item.name}
-								>
-									{item.name}
-								</MenuItem>
-							))}
+							{sortedList
+								.sort((a, b) => (a.name > b.name ? 1 : -1))
+								.map((item) => (
+									<MenuItem
+										style={{ display: 'flex' }}
+										key={item.name}
+										value={item.name}
+									>
+										{item.name}
+									</MenuItem>
+								))}
 						</TextField>
 					</div>
 				</Box>
